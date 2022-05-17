58b8bedb877be491edb08608dde84d62c72fac3b
Alex Yang <alex.yang1115@gmail>
2022-05-17 15:25:25 +0100

    refactored to use sql aggregate functions and group by

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -7,7 +7,6 @@ exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleById(article_id)
     .then((article) => {
-      console.log(article);
       res.status(200).send({ article });
     })
     .catch((err) => {
