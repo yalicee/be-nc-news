d84ec95a09eef1df5c39c915f449876e149921c6
Alex Yang <alex.yang1115@gmail>
2022-05-17 13:17:28 +0100

    refactored error handles into errors.controllers.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const {
   patchArticle,
   getArticleById,
 } = require("./controllers/articles.controllers");
+const {
+  handlesPSQLErrors,
+  handlesCustomErrors,
+  handlesInternalServerErrors,
+  handlesNotFoundErrors,
+} = require("./controllers/errors.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
 const { getUsers } = require("./controllers/users.controllers");
 
@@ -18,28 +24,12 @@ app.patch("/api/articles/:article_id", patchArticle);
 
 app.get("/api/users", getUsers);
 
-app.use("/*", (req, res, next) => {
-  res.status(404).send({ msg: "not found" });
-});
+app.use("/*", handlesNotFoundErrors);
 
-app.use((err, req, res, next) => {
-  if (err.code) {
-    res.status(400).send({ msg: "bad request" });
-  } else {
-    next(err);
-  }
-});
+app.use(handlesPSQLErrors);
 
-app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
-});
+app.use(handlesCustomErrors);
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ msg: "internal server error" });
-});
+app.use(handlesInternalServerErrors);
 
 module.exports = app;
