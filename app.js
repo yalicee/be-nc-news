44894a4e40aca56cf69da2d97b7e9902a9ceec2a
Alex Yang <alex.yang1115@gmail>
2022-06-13 10:35:51 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const {
